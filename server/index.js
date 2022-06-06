b1acf9aafaf4de58e86286db95e9fd3a6a2775ca
LucasTamayaGIT <lucastamaya.974@gmail.com>
2022-06-06 12:07:21 +0400

    update endpoint url 3

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://instagram-clone-fullstack-lucastamaya.vercel.app",
+    origin: "https://instagram-clone-fullstack-lucastamaya.vercel.app/",
     credentials: true,
   })
 );
